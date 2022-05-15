df0b1e1dd9a4d8fd18bf5975cb9a7d93b47c9c14
greatstep93 <greatstep93@gmail.com>
2022-05-15 22:00:07 +0400

    Задача 2.1.4

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
 public class Needle7 {
+
+    private Deth8 death;
+    @Autowired
+    public Needle7(Deth8 death){
+        this.death = death;
+    }
 
     @Override
     public String toString() {
